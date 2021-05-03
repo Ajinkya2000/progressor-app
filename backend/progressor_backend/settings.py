91f128fa8096bfeb1a10e1e70685c95a42a5e54c
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-05-04 00:43:49 +0530

    add static_root file

--- backend/progressor_backend/settings.py
+++ backend/progressor_backend/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
